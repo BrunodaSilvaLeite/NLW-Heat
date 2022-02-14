671d20d48782bcc083d0f001e5d45192edef5bf5
Bruno <bruno.etiel.silva@gmail.com>
2022-02-14 11:18:06 -0300

    feat(main): Add main

---
+++ front/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from '../src/App'
+import { AuthProvider } from './context/auth'
+
+import "./styles/global.scss"
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
