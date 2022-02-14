c88929f46d0549b40a771be86ef87684b65859f9
Bruno <bruno.etiel.silva@gmail.com>
2022-02-14 11:11:51 -0300

    feat(ProfileUserController): Add ProfileUserController

---
+++ back/src/controller/ProfileUserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { ProfileUserService } from "../services/ProfileUserService";
+
+class ProfileUserController {
+    async handle(req: Request, res: Response) {
+
+        const {user_id} = req;
+
+        const service = new ProfileUserService();
+
+        try {
+            const result = await service.execute(user_id);
+
+            return res.json(result)
+
+        } catch (err) {
+            return res.json({ error: err.message });
+        }
+
+    }
+}
+
+export { ProfileUserController }
