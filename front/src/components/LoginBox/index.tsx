8e39f765abc561caa296eba58bb5e9b51021db1d
Bruno <bruno.etiel.silva@gmail.com>
2022-02-14 11:22:18 -0300

    feat(LoginBox): Add LoginBox

---
+++ front/src/components/LoginBox/index.tsx
@@ -0,0 +1,18 @@
+import styles from "./styles.module.scss"
+import { VscGithubInverted } from "react-icons/vsc"
+import { useContext } from "react"
+import { AuthContext } from "../../context/auth"
+
+export function LoginBox() {
+    const { signInUrl, user} = useContext(AuthContext)
+  
+    return (
+        <div className={styles.loginBoxWrapper}>
+            <strong>Entre e compartilhe sua mensagem</strong>
+            <a href={signInUrl} className={styles.signInWithGithub}>
+            <VscGithubInverted size="24"/> 
+            Entrar com Github
+            </a>
+        </div>
+    )
+};
